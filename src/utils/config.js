b8b4cf6d5e49c27c9248c54b98fc6c6b1ebf20b4
Ruhulcse <Ruhul.cse7862@gmail.com>
2021-05-29 21:56:20 +0600

    config file

--- src/utils/config.js
+++ src/utils/config.js
@@ -3,5 +3,4 @@ let token = localStorage.getItem("token");
 export const config = {
   headers: { Authorization: `Bearer ${token}` },
 };
-export const URL = "http://localhost:5000/";
-//"https://cloudbaseddataentry.herokuapp.com/";
+export const URL = "https://cloud7862.herokuapp.com/";
